6e73d0ed0c40247c9ad6fde87a0488078f0995ee
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-10-20 15:52:18 +0300

    Feed nav complete

--- components/Feed.js
+++ components/Feed.js
@@ -1,10 +1,15 @@
+import { SparklesIcon } from "@heroicons/react/outline";
 
 const Feed = () => {
   return (
-    <div>
-      <p>Feed component</p>
-    </div>
-  )
-}
+    <main>
+      <nav className="flex justify-between items-center h-[3rem] border-b-[1px] cursor-pointer">
+        <h1 className="font-bold ml-4 text-lg">Home</h1>
+        <SparklesIcon className="h-6 w-6 mr-4 "/>
+      </nav>
+      
+    </main>
+  );
+};
 
-export default Feed
+export default Feed;
