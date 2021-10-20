d1ea6356cdbc3a54e7371c410333fdc2c2ed5246
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-10-20 17:08:17 +0300

    Tweets form complete.

--- components/Feed.js
+++ components/Feed.js
@@ -1,13 +1,57 @@
-import { SparklesIcon } from "@heroicons/react/outline";
+import {
+  SparklesIcon,
+  PhotographIcon,
+  GiftIcon,
+  EmojiHappyIcon,
+  CalendarIcon,
+} from "@heroicons/react/outline";
 
 const Feed = () => {
   return (
     <main>
-      <nav className="flex justify-between items-center h-[3rem] border-b-[1px] cursor-pointer">
+      <nav className="sticky top-0 flex justify-between items-center h-[3rem] border-b-[1px] cursor-pointer">
         <h1 className="font-bold ml-4 text-lg">Home</h1>
-        <SparklesIcon className="h-6 w-6 mr-4 "/>
+        <SparklesIcon className="h-6 w-6 mr-4 " />
       </nav>
-      
+      {/* tweets section */}
+      <section className="w-full flex  border-b-[1px]">
+        <img
+          className="h-12 w-12 rounded-full m-4 "
+          src="https://scontent.fnbo9-1.fna.fbcdn.net/v/t1.6435-9/39700554_189956408607706_3333751201383579648_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeGsHiqUtc6rhY_8qkhFd_CADsuEpp0tV7sOy4SmnS1XuzWsDZ-MDY-eDp_Ugr8KX-dF3OBwZ1uIIWz-3Jku2-RW&_nc_ohc=zqLWdK0KzkgAX9dQNYA&_nc_pt=5&_nc_ht=scontent.fnbo9-1.fna&oh=813ab2a5c2b58fb29999df2114c95a8a&oe=619741AD"
+          alt=""
+        />
+        <div className="flex-1 mr-4">
+          <form>
+            <textarea
+            placeholder="What's happening? "
+              multiple
+              rows="1"
+              className="p-3 text-xl w-full focus:outline-none  focus:border-transparent scrollbar-hide"
+              type="text"
+            />
+
+            <section className="flex items-center justify-between border-t-[1px]">
+              <div className="flex items-center ">
+                <span className="feedTweetSpan">
+                  <PhotographIcon className="feedTweetIcons " />
+                </span>
+                <span className="feedTweetSpan">
+                  <GiftIcon className="feedTweetIcons " />
+                </span>
+                <span className="feedTweetSpan">
+                  <EmojiHappyIcon className="feedTweetIcons " />
+                </span>
+                <span className="feedTweetSpan">
+                  <CalendarIcon className="feedTweetIcons " />
+                </span>
+              </div>
+              <button className=" bg-blue-500 h-10 w-20 text-sm  m-2 text-white p-2 rounded-full hover:bg-blue-600">
+                Tweet
+              </button>
+            </section>
+          </form>
+        </div>
+      </section>
     </main>
   );
 };
